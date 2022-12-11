9b1456eaccae0bf48e8079fff826c31b3bb26695
aquacalc <aquacalc@yahoo.com>
2022-12-11 18:50:41 -0500

    initial commit

---
+++ src/components/Layout.js
@@ -0,0 +1,8 @@
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  // see: (~2:15:00) https://www.youtube.com/watch?v=CvCiNeLnZ00&t=27019s
+  return <Outlet />;
+};
+
+export default Layout;
