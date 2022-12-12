efb606f761741f399f3eea7bdc6dda5d2ce12233
aquacalc <aquacalc@yahoo.com>
2022-12-11 20:25:55 -0500

    add Dashlayout

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Public from "./components/Public";
 import Login from "./components/Login";
+import Dashlayout from "./components/Dashlayout";
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Public />} />
-          <Route path='login' element={<Login />} />
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
+        <Route path="dash" element={<Dashlayout />}>
+          yadda
         </Route>
-      </Routes>
+      </Route>
+    </Routes>
   );
 }
 
