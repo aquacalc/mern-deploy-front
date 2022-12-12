743034958829d378873f2c4fbaa409d7be2d25e4
aquacalc <aquacalc@yahoo.com>
2022-12-11 21:31:21 -0500

    add features directory

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Public from "./components/Public";
-import Login from "./components/Login";
+import Login from "./features/auth/Login";
 import Dashlayout from "./components/Dashlayout";
+import Welcome from "./features/auth/Welcome";
+import NotesList from "./features/notes/NotesList";
+import UsersList from "./features/users/UsersList";
 
 function App() {
   return (
@@ -10,9 +13,18 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
+        
         <Route path="dash" element={<Dashlayout />}>
-          yadda
-        </Route>
+          <Route index element={<Welcome />} />
+          <Route path="/users" element={<UsersList />} />
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+          </Route>
+          <Route path="users">
+            <Route index element={<UsersList />} />
+          </Route>
+        </Route> {/* END DASH */}
+
       </Route>
     </Routes>
   );
---
+++ src/features/notes/NotesList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotesList = () => {
+  return (
+    <hi>NotesList</hi>
+  )
+}
+
+export default NotesList
